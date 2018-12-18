376eee238ad72828fc6ffacd9eb3bef55ce5a4ba
Serzh Nosov <Serzh.Nosov@lanit-tercom.com>
2018-12-18 17:30:55 +0300

    refactoring structure of folder

---
+++ src/core/examples/folder2/src/test/java/source/Task1Test.java
@@ -0,0 +1,13 @@
+package source;
+
+import org.junit.Test;
+
+public class Task1Test {
+
+    @Test
+    public void task1() {
+        //Task1.run();
+        System.out.println("HERER");
+    }
+
+}
